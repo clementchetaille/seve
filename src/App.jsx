e2a344510e5afa40fa59f842ca7eb01583212b03
Clement Chetaille <clement.chetaille@gmail.com>
2026-04-13 14:52:40 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Concept from './components/Concept'
+import Histoire from './components/Histoire'
+import Menu from './components/Menu'
+import Ambiance from './components/Ambiance'
+import Reservation from './components/Reservation'
+import Footer from './components/Footer'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Concept />
+      <Menu />
+      <Histoire />
+      <Ambiance />
+      <Reservation />
+      <Footer />
+    </>
+  )
+}
